30f841066b01156443c60a16e4e293d4e2f1d9c0
Christopher Albers-Hardebeck <calber32@thomasmore.edu>
2020-09-23 15:18:45 -0400

    Date Picker added

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
